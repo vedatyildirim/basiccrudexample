4a17ad12a2909d52082000ee1318e98738917bc3
Vedat Yıldırım <Vedat.Yilidrim@vodafone.com>
2019-12-30 17:40:06 +0300

    Düzenlendi

--- src/test/java/com/vedatyildirim/basiccrudexample/BasiccrudexampleApplicationTests.java
+++ src/test/java/com/vedatyildirim/basiccrudexample/BasiccrudexampleApplicationTests.java
@@ -1,13 +1,20 @@
 package com.vedatyildirim.basiccrudexample;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.vedatyildirim.basiccrudexample.controller.PostController;
+import org.junit.jupiter.api.Test;
 
-//@SpringBootTest
+@SpringBootTest
 class BasiccrudexampleApplicationTests {
 
+	@Autowired
+	private PostController controller;
+
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
